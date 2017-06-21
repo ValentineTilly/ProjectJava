51bd9a29444286f783cd61f221d7b662d6c2d23b
Valentine <Valentine@PC-Valentine>
2017-06-21 11:27:55 +0200

    CompteurDiamant

---
+++ BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/dao/CompteurDiamant.java
@@ -0,0 +1,33 @@
+/*package model.dao;
+
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CompteurDiamant extends AbstractDAO {
+
+	
+	private static int nombreDiamant = 0;
+	
+	public static int getNbrDiamants () throws SQLException {
+    	String sql = "{call count_diamants()}"; 
+    	CallableStatement callStatement = prepareCall(sql);
+    	int nombreDiamant = 0;
+    	
+    	if (callStatement.execute()) {
+            final ResultSet result = callStatement.getResultSet();
+            if (result.first()) {
+                for ( boolean isResultLeft = result.first() ; isResultLeft ; isResultLeft = result.next()) {
+                	for (int i = 0; i < 5; i++) {
+						if (result.getInt(i+1) == 2) {
+							nombreDiamant++;
+						}
+					}
+                }
+            }
+               result.close();
+        }
+        System.out.println("Diamants" + nombreDiamant);
+     	return nombreDiamant;
+    }
+}*/
